357db4f6efe90d51d302aa6705250bb8582057b0
Extex101 <33675971+Extex101@users.noreply.github.com>
2020-03-20 14:29:46 -0700

    Add MineClone support and remove liquids_pointable

--- init.lua
+++ init.lua
@@ -6,7 +6,6 @@ dofile(path.."/api.lua")
 minetest.register_tool("light_tool:light_tool", {
 	description = "Light Tool",
 	inventory_image = "light_tool_light_tool.png",
-	liquids_pointable = true,
 })
 light_tool.add_tool("light_tool:light_tool", 20)
 
@@ -50,7 +49,18 @@ if minetest.get_modpath("default") then
 		},
 	})
 end
-
+if minetest.get_modpath("mcl_core") then
+	light_tool.register_glow_node("mcl_core:water_source")
+	light_tool.register_glow_node("mcl_core:water_flowing")
+	minetest.register_craft({
+		output = "light_tool:light_tool",
+		recipe = {
+			{"","mcl_mobitems:blaze_rod","mcl_nether:glowstone"},
+			{"mcl_mobitems:blaze_rod","mcl_core:steel_ingot","mcl_mobitems:blaze_rod"},
+			{"mcl_core:steel_ingot", "mcl_mobitems:blaze_rod",""},
+		},
+	})
+end
 minetest.register_globalstep(function()
 	for _, user in ipairs(minetest.get_connected_players()) do
 		local stack = ItemStack(user:get_wielded_item())
@@ -58,8 +68,8 @@ minetest.register_globalstep(function()
 		if light_tool.check(light_tool.tools, wielded.name) then
 			local index = light_tool.check_index(light_tool.tools, wielded.name)
 			local dir = user:get_look_dir()
-	        local pos = user:get_pos()
-	        light_tool.light_beam({x = pos.x, y = pos.y+1, z = pos.z}, dir, light_tool.range[index])
+			local pos = user:get_pos()
+			light_tool.light_beam({x = pos.x, y = pos.y+1, z = pos.z}, dir, light_tool.range[index])
 		end
 	end
 end)
